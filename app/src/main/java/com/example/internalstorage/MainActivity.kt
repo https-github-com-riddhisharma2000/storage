d2c94a5a66d02bc5a9560fdb83cf200f2e35d271
shruti <shrutibansal1802@gmail.com>
2019-09-14 15:19:56 +0530

    internal storage

--- app/src/main/java/com/example/internalstorage/MainActivity.kt
+++ app/src/main/java/com/example/internalstorage/MainActivity.kt
@@ -13,7 +13,7 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-
+//hello
 class MainActivity: AppCompatActivity() {
     // coding180.c0m
     override fun onCreate (savedInstanceState: Bundle?) {
